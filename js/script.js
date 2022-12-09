cf9784d8f0d58114f177287ac4054efdfea62eae
Akhazogie Victoria <akhazogievictoria2@gmail.com>
2022-12-09 12:48:31 +0100

    add business logic

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const num1 = parseInt(prompt("Enter a number:"));
+const num2 = parseInt(prompt("Enter another number:"));
+const result1 = add(num1, num2);
+alert(result1);
+
+function subtract(num3, num4) {
+  return num3 - num4;
+}
+
+const num3 = parseInt(prompt("Enter a number:"));
+const num4 = parseInt(prompt("Enter another number:"));
+const result2 = subtract(num3, num4);
+alert(result2);
+
+function multiply(num5, num6) {
+  return num5 * num6;
+}
+
+const num5 = parseInt(prompt("Enter a number:"));
+const num6 = parseInt(prompt("Enter another number:"));
+const result3 = multiply(num5, num6);
+alert(result3);
+
+function divide(num7, num8) {
+  return num7 / num8;
+}
+
+const num7 = parseInt(prompt("Enter a number:"));
+const num8 = parseInt(prompt("Enter another number:"));
+const result4 = divide(num7, num8);
+alert(result4);
